0586c4afb100b4c48b5772348c44ddab0d997057
RodCri <cristian.rodriguez1317@gmail.com>
2023-09-14 11:22:39 -0500

    add section services  with slider

--- js/main.js
+++ js/main.js
@@ -9,3 +9,36 @@ hamburguer.addEventListener('click',()=>{
     navOptions.classList.remove('show');
   }
 })
+
+const swiper = document.querySelector('.mySwiper');
+const swiperParams = {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    340:{
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    980: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1300: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+  on: {
+    init() {
+      // ...
+    },
+  },
+};
+
+Object.assign(swiper, swiperParams);
+
+swiper.initialize();
